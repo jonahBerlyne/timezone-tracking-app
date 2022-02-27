ffeabebfea3aeb2bee7bcb77797a5c8a6f8b0c3e
jonahBerlyne <jonshaber@gmail.com>
2022-02-27 15:35:17 -0500

    added register and login forms

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,7 +1,42 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import fireDB from "../firebaseConfig";
+
 export default function Login() {
+ const [email, setEmail] = useState('');
+ const [password, setPassword] = useState('');
+ const auth = getAuth();
+
+ const login = async () => {
+  try {
+   const result = await signInWithEmailAndPassword(auth, email, password);
+   localStorage.setItem("currentUser", JSON.stringify(result));
+   alert("Logged in");
+   window.location.href = '/';
+  } catch (err) {
+   alert(`Error: ${err}`);
+  }
+ }
+
  return (
   <div>
-   Login
+   <h2>Login:</h2>
+   <input 
+    type="email" 
+    className='form-control' placeholder='Email' 
+    value={email} 
+    onChange={(e) => {setEmail(e.target.value)}}
+   />
+   <input 
+    type="password" 
+    className='form-control' placeholder='Password' 
+    value={password} 
+    onChange={(e) => {setPassword(e.target.value)}}
+   />
+   <button className='my-3' onClick={login}>Login</button>
+   <hr/>
+   <Link to="/register">Click Here to Register</Link>
   </div>
  );
 }
--- src/Auth/Register/RegisterForm.js
+++ src/Auth/Register/RegisterForm.js
@@ -1,7 +1,49 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import fireDB from "../../firebaseConfig";
+
 export default function RegisterForm() {
+ const [email, setEmail] = useState('');
+ const [password, setPassword] = useState('');
+ const [confirmPassword, setConfirmPassword] = useState('');
+ const auth = getAuth();
+
+ const register = async () => {
+  if (password !== confirmPassword) return;
+  try {
+   const result = await createUserWithEmailAndPassword(auth, email, password);
+   localStorage.setItem("currentUser", JSON.stringify(result));
+   alert("Registered");
+   window.location.href = '/';
+  } catch (err) {
+   alert(`Registration error: ${err}`);
+  }
+ }
  return (
   <div>
-   RegisterForm
+   <h2>Register:</h2>
+   <input 
+    type="email" 
+    className='form-control' placeholder='Email' 
+    value={email} 
+    onChange={(e) => {setEmail(e.target.value)}}
+   />
+   <input 
+    type="password" 
+    className='form-control' placeholder='Password' 
+    value={password} 
+    onChange={(e) => {setPassword(e.target.value)}}
+   />
+   <input 
+    type="password" 
+    className='form-control' placeholder='Confirm Password' 
+    value={confirmPassword} 
+    onChange={(e) => {setConfirmPassword(e.target.value)}}
+   />
+   <button className='my-3' onClick={register}>Register</button>
+   <hr/>
+   <Link to="/login">Click Here to Login</Link>
   </div>
  );
 }
