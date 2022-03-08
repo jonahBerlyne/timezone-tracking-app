a7ead446001f6d4c51bd4b2064799a26d083317a
jonahBerlyne <jonshaber@gmail.com>
2022-03-07 20:20:21 -0500

    added timezone api to edit profile div

--- src/User/Profile/EditProfile.js
+++ src/User/Profile/EditProfile.js
@@ -1,17 +1,41 @@
-export default function EditProfile({ values, handleChange, choosePic, imgFileErr }) {
+export default function EditProfile({ values, handleChange, choosePic, imgFileErr, countries, zones, showZones }) {
+
  return (
   <div>
    <div>
+    <h4>Upload a new profile pic:</h4>
     <input onChange={choosePic} type="file"/>
     {imgFileErr && <h6>{imgFileErr}</h6>}
    </div>
    <div>
-    <label>Name:</label>
-    <input type="text" name="name" value={values.name} onChange={handleChange}/>
+    <h4>Type a new name:</h4>
+    <input type="text" name="name" value={values.name} onChange={handleChange} placeholder={values.name}/>
    </div>
    <div>
-    <label>Type here to search for your location:</label>
-    <input type="text" name="location" value={values.location} onChange={handleChange}/>
+    <h4>Select a new country:</h4>
+    <select id="countryBox" name="country" onChange={handleChange}>
+     <option defaultValue="" key=""></option>
+     {countries.map(country => {
+      return (
+       <option key={countries.indexOf(country)}>{country}</option>
+      );
+     })}
+    </select>
+    <br/>
+    {showZones &&
+     <div>
+      <h4>Select a new timezone:</h4>
+      <select id="timezoneBox" name="timezone" onChange={handleChange} required>
+       <option defaultValue="" key=""></option>
+       {zones.map(zone => {
+        return (
+         <option key={zones.indexOf(zone)}>{zone}</option>
+        );
+       })}
+      </select>
+     </div>
+    }
+    <br/>
    </div>
   </div>
  );
--- src/User/Profile/ProfilePage.js
+++ src/User/Profile/ProfilePage.js
@@ -23,14 +23,75 @@ export default function ProfilePage() {
   setShowProfile(true);
  }
 
- const [values, setValues] = useState({name: user.userInfo.name, location: user.userInfo.location});
+ const [values, setValues] = useState({name: "", country: "", timezone: ""});
+
+ const [profileZone, setProfileZone] = useState("");
+
+ useEffect(() => {
+  let zoneStr = user.userInfo.timezoneData.zoneName;
+  zoneStr = zoneStr.substring(zoneStr.indexOf("/") + 1, zoneStr.length).replace(/_+/g, ' ');
+  setProfileZone(zoneStr);
+  fetchAPI();
+ }, []);
+
+ const [countries, setCountries] = useState([]);
+ const [zones, setZones] = useState([]);
+ const [zonesConst, setZonesConst] = useState([]);
+ const [zoneData, setZoneData] = useState([]);
+
+ const fetchAPI = async () => {
+  try {
+   const apiKey = process.env.REACT_APP_TIMEZONE_API_KEY;
+   const data = await fetch(`http://api.timezonedb.com/v2.1/list-time-zone?key=${apiKey}&format=json`);
+   const dataJSON = await data.json();
+   setZoneData(dataJSON.zones);
+   let countriesArr = [];
+   let zonesArr = [];
+   dataJSON.zones.forEach(zone => {
+    countriesArr.push(zone.countryName);
+    zonesArr.push(zone);
+   });
+   setCountries([...new Set(countriesArr)].sort());
+   setZonesConst(zonesArr);
+  } catch (err) {
+   alert(`Fetching error: ${err}`);
+  }
+ }
+
+ const [showZones, setShowZones] = useState(false);
 
  const handleChange = e => {
-  setValues({
-   ...values,
-   [e.target.name]: e.target.value
-  });
+  if (e.target.name === "country") {
+   setValues({
+    ...values,
+    [e.target.name]: e.target.value,
+    timezone: ""
+   });
+   setShowZones(false);
+   if (e.target.value !== "") {
+    setTimeout(() => {
+     setShowZones(true);
+    }, 0.0000000000000000001);
+   }
+  } else {
+   setValues({
+    ...values,
+    [e.target.name]: e.target.value
+   });
+  }
  }
+
+ useEffect(() => {
+  if (values.country !== "") {
+   const zonesCopy = zonesConst;
+   const timezones = zonesCopy.filter(zone => zone.countryName === values.country);
+   let timezonesArr = [];
+   timezones.forEach(timezone => {
+    timezonesArr.push(timezone.zoneName);
+   });
+   setZones(timezonesArr);
+  }
+ }, [values]);
 
  const [imgFile, setImgFile] = useState(null);
  const [imgFileErr, setImgFileErr] = useState(null);
@@ -68,19 +129,28 @@ export default function ProfilePage() {
  const [refresh, setRefresh] = useState(false);
 
  const saveChanges = async () => {
+  if (imgUrl === null && values.name === "" && values.country === "") {
+   alert(`You haven't made any changes, ${user.userInfo.name}.`);
+   return;
+  }
+  if (showZones && values.timezone === "") {
+   alert("Please select a new timezone before saving.");
+   return;
+  }
   try {
    await handleUpload();
    const docRef = doc(fireDB, "users", `${user.uid}`);
+   const userZoneData = zoneData.filter(zone => zone.zoneName === values.timezone);
    const userInfo = {
     id: user.uid,
-    name: values.name,
-    location: values.location,
+    name: values.name !== "" ? values.name : user.userInfo.name,
+    timezoneData: values.country !== "" && values.timezone !== "" ? userZoneData[0] : user.userInfo.timezoneData,
     profilePic: imgUrl
    };
    await setDoc(docRef, userInfo);
    alert("Changes saved");
    localStorage.setItem("currentUser", JSON.stringify({...user, userInfo}));
-   setRefresh(!refresh);
+   window.location = `/profile/${user.uid}`;
   } catch (err) {
    alert(`Change saving error: ${err}`);
   }
@@ -90,13 +160,13 @@ export default function ProfilePage() {
   <div>
    {showProfile &&
     <div>
-     <Profile name={user.userInfo.name} location={user.userInfo.location} imgUrl={imgUrl}/>
+     <Profile name={user.userInfo.name} zoneName={profileZone} imgUrl={imgUrl}/>
      <button onClick={goToEditProfile}>Edit your profile or location</button>
     </div> 
    }
    {editProfile &&
     <div>
-     <EditProfile values={values} handleChange={handleChange} choosePic={choosePic} imgFileErr={imgFileErr}/>
+     <EditProfile values={values} handleChange={handleChange} choosePic={choosePic} imgFileErr={imgFileErr} countries={countries} zones={zones} showZones={showZones}/>
      <button onClick={goToProfile}>Go back</button>
      <button onClick={saveChanges}>Save</button>
     </div>
