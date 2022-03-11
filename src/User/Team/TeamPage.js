a6f334cd0b7829df4a1fccc5efdb65949ccde839
jonahBerlyne <jonshaber@gmail.com>
2022-03-10 19:51:57 -0500

    retrieved team members data for team page

--- src/User/Team/TeamPage.js
+++ src/User/Team/TeamPage.js
@@ -1,4 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import fireDB from "../../firebaseConfig";
+import { doc, getDocs, query, collection } from "firebase/firestore";
+import { useParams } from 'react-router-dom';
+
 export default function TeamPage() {
+
+ const user = JSON.parse(localStorage.getItem("currentUser"));
+ const team = useParams();
+
+ const getTeamMembers = async () => {
+  try {
+   const membersCollection = query(collection(fireDB, "users", `${user.uid}`, "teams", `${team.id}`, "team_members"));
+   const membersSnapshot = await getDocs(membersCollection);
+   membersSnapshot.forEach(member => {
+    console.log(member.data());
+   });
+  } catch (err) {
+   alert(`Team members retrieval error: ${err}`);
+  }
+ }
+
+ useEffect(() => {
+  getTeamMembers();
+ }, []);
+
  return (
   <div>
    TeamPage
