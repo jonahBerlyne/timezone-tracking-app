3613dba20838204ef6077e49dbe5012cfacee955
jonahBerlyne <jonshaber@gmail.com>
2022-04-23 16:42:33 -0400

    created auth route

---
+++ src/Routes/AuthRoute.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
+
+export default function AuthRoute ({children}) {
+ const [pending, setPending] = useState(true);
+ const [currentUser, setCurrentUser] = useState(null);
+ const auth = getAuth();
+
+ useEffect(() => {
+  const unsub = onAuthStateChanged(
+   auth,
+   user => {
+    user ? setCurrentUser(user) : setCurrentUser(null);
+    setPending(false);
+   },
+   err => {
+    alert(`Error: ${err}`);
+    setPending(false);
+   }
+  );
+
+  return unsub;
+ }, []);
+
+ if (pending) return null;
+
+ if (!currentUser) {
+   return children;
+ } else {
+   return <Navigate to="/"/>;
+ }
+}
