74805c14b05fd967edb181d8a656f29e042b00c3
jonahBerlyne <jonshaber@gmail.com>
2022-04-26 21:10:54 -0400

    added types to auth route

--- src/Routes/AuthRoute.tsx
+++ src/Routes/AuthRoute.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, User } from 'firebase/auth';
 import { getAuth } from 'firebase/auth';
 import AuthNavbar from '../Auth/AuthNavbar';
 
 export default function AuthRoute ({children}: {children: any}) {
- const [pending, setPending] = useState(true);
- const [currentUser, setCurrentUser] = useState<any>(null);
+ const [pending, setPending] = useState<boolean>(true);
+ const [currentUser, setCurrentUser] = useState<User | null>(null);
  const auth = getAuth();
 
  useEffect(() => {
