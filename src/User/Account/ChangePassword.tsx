aa17d121ae0cb958c4c2f3732d140f32564e67dc
jonahBerlyne <jonshaber@gmail.com>
2022-04-27 18:46:52 -0400

    added types to change user password

--- src/User/Account/ChangePassword.tsx
+++ src/User/Account/ChangePassword.tsx
@@ -2,35 +2,45 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { EmailAuthProvider, getAuth, reauthenticateWithCredential, updatePassword } from 'firebase/auth';
 
+interface Values {
+ current: string;
+ new: string;
+ confirmNew: string;
+};
+
 export default function ChangePassword() {
 
  const auth = getAuth();
 
  const initialValues = { current: '', new: '', confirmNew: '' };
 
- const [values, setValues] = useState(initialValues);
+ const [values, setValues] = useState<Values>(initialValues);
 
- const handleChange = e => {
+ const handleChange = (e: any): void => {
   setValues({
    ...values,
    [e.target.name]: e.target.value
   });
  }
 
- const changeMyPassword = async () => {
+ const changeMyPassword = async (): Promise<any> => {
   if (values.new !== values.confirmNew) {
    alert("Make sure to confirm your new password");
    return;
   }
   try {
    const currentUser = auth.currentUser;
-   const credential = EmailAuthProvider.credential(
-    auth.currentUser.email,
-    values.current
-   );
-   await reauthenticateWithCredential(currentUser, credential);
-   await updatePassword(currentUser, values.new);
-   alert("Password updated");
+   if (currentUser) {
+    const credential = EmailAuthProvider.credential(
+     currentUser.email!,
+     values.current
+    );
+    await reauthenticateWithCredential(currentUser, credential);
+    await updatePassword(currentUser, values.new);
+    alert("Password updated");
+   } else {
+    return;
+   }
    window.location.href = "/account";
   } catch (err) {
    alert(`Password change error: ${err}`);
