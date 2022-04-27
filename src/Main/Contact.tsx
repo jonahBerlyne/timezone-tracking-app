6dc17e52ccae9725790786ee1965356ee9e6c3bf
jonahBerlyne <jonshaber@gmail.com>
2022-04-26 21:07:21 -0400

    added types to contact form

--- src/Main/Contact.tsx
+++ src/Main/Contact.tsx
@@ -2,20 +2,27 @@ import React, { useState } from 'react';
 import fireDB from "../firebaseConfig";
 import { doc, setDoc } from 'firebase/firestore';
 
+interface Values {
+ name: string;
+ email: string;
+ subject: string;
+ message: string;
+}
+
 export default function Contact() {
 
- const initialValues = {name: "", email: "", subject: "", message: ""};
+ const initialValues: Values = {name: "", email: "", subject: "", message: ""};
 
- const [values, setValues] = useState(initialValues);
+ const [values, setValues] = useState<Values>(initialValues);
 
- const handleChange = e => {
+ const handleChange = (e: any): void => {
   setValues({
    ...values,
    [e.target.name]: e.target.value
   });
  }
 
- const sendForm = async () => {
+ const sendForm = async (): Promise<any> => {
   const docRef = doc(fireDB, "messages", `${Date()}`);
   await setDoc(docRef, values);
   alert("Message sent");
@@ -29,7 +36,7 @@ export default function Contact() {
     <input type="text" name="name" value={values.name} onChange={handleChange} placeholder="Your name" required></input>
     <input type="email" name="email" value={values.email} onChange={handleChange} placeholder="Your email" required></input>
     <input type="text" name="subject" value={values.subject} onChange={handleChange} placeholder="Your subject" required></input>
-    <textarea rows="10" cols="40" name="message" value={values.message} onChange={handleChange} placeholder="Your message" required/>
+    <textarea rows={10} cols={40} name="message" value={values.message} onChange={handleChange} placeholder="Your message" required/>
    </form>
    <button onClick={sendForm}>Send</button>
   </div>
