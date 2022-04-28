4a472eebe47df9f9a394e271029804e6cef812b7
jonahBerlyne <jonshaber@gmail.com>
2022-04-27 20:01:24 -0400

    added types to teams page

--- src/User/Team/TeamsPage.tsx
+++ src/User/Team/TeamsPage.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function TeamsPage() {
 
- const teams = JSON.parse(localStorage.getItem("teams"));
+ const teams = JSON.parse(localStorage.getItem("teams") || "{}");
  
  return (
   <div>
    {teams.length === 0 && <h1>You haven't added any teams, yet.</h1>}
    {teams.length > 0 && <h1>Here are your teams:</h1>}
-   {teams && teams.map(team => {
+   {teams && teams.map((team: any) => {
     return (
      <div key={team.id}>
       <Link to={`/team/${team.id}`}>{team.name}</Link>
